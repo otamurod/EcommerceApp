e4cb944d3d799308c7a5e957b71ae2520e360c5e
otamurod <o.safarov@student.inha.uz>
2024-04-05 10:26:32 +0500

    Project Setup

---
+++ app/src/main/java/uz/otamurod/ecommerceapp/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package uz.otamurod.ecommerceapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.otamurod.ecommerceapp.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
