61f094f16a5ea797dbab99a05421443d2eb264c9
otamurod <o.safarov@student.inha.uz>
2024-04-06 17:11:27 +0500

    feat: implement Register functionality

---
+++ app/src/main/java/uz/otamurod/ecommerceapp/data/User.kt
@@ -0,0 +1,10 @@
+package uz.otamurod.ecommerceapp.data
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    var imagePath: String = ""
+) {
+    constructor() : this("", "", "", "")
+}
--- app/src/main/java/uz/otamurod/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/uz/otamurod/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
@@ -1,60 +1,72 @@
 package uz.otamurod.ecommerceapp.fragments.loginRegister
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import uz.otamurod.ecommerceapp.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import uz.otamurod.ecommerceapp.data.User
+import uz.otamurod.ecommerceapp.databinding.FragmentRegisterBinding
+import uz.otamurod.ecommerceapp.util.Resource
+import uz.otamurod.ecommerceapp.viewmodel.RegisterViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class RegisterFragment : Fragment(R.layout.fragment_register) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+@AndroidEntryPoint
+class RegisterFragment : Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
+    private val viewModel by viewModels<RegisterViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonRegister.setOnClickListener {
+                val user = User(
+                    editTextFirstNameRegister.text.toString().trim(),
+                    editTextLastNameRegister.text.toString().trim(),
+                    editTextEmailRegister.text.toString().trim()
+                )
+
+                val password = editTextPasswordRegister.text.toString()
+
+                viewModel.createAccountWithEmailAndPassword(user, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.register.collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.buttonRegister.startAnimation()
+                    }
+
+                    is Resource.Success -> {
+                        Log.d(TAG, "onViewCreated: ${it.data.toString()}")
+                        binding.buttonRegister.revertAnimation()
+                    }
+
+                    is Resource.Error -> {
+                        Log.e(TAG, "onViewCreated: ${it.message.toString()}")
+                        binding.buttonRegister.revertAnimation()
+                    }
+                    else -> Unit
+                }
+            }
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val TAG = "RegisterFragment"
     }
 }
