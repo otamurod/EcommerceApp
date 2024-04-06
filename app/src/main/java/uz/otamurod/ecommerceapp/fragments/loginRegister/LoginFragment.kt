b6c79efce2596118b123b125d70cc7232bc5515d
otamurod <o.safarov@student.inha.uz>
2024-04-06 20:09:29 +0500

    feat: implement Login functionality

--- app/src/main/java/uz/otamurod/ecommerceapp/fragments/loginRegister/LoginFragment.kt
+++ app/src/main/java/uz/otamurod/ecommerceapp/fragments/loginRegister/LoginFragment.kt
@@ -1,60 +1,74 @@
 package uz.otamurod.ecommerceapp.fragments.loginRegister
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import uz.otamurod.ecommerceapp.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import uz.otamurod.ecommerceapp.activities.ShoppingActivity
+import uz.otamurod.ecommerceapp.databinding.FragmentLoginBinding
+import uz.otamurod.ecommerceapp.util.Resource
+import uz.otamurod.ecommerceapp.viewmodel.LoginViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LoginFragment : Fragment(R.layout.fragment_login) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel by viewModels<LoginViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonLogin.setOnClickListener {
+                val email = editTextEmailLogin.text.toString().trim()
+                val password = editTextPasswordLogin.text.toString()
+
+                viewModel.login(email, password)
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.login.collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.buttonLogin.startAnimation()
+                    }
+
+                    is Resource.Success -> {
+                        binding.buttonLogin.revertAnimation()
+
+                        Intent(requireActivity(), ShoppingActivity::class.java).also { intent ->
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                        }
+                    }
+
+                    is Resource.Error -> {
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                        binding.buttonLogin.revertAnimation()
+                    }
+
+                    else -> Unit
+                }
+            }
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val TAG = "LoginFragment"
     }
 }
