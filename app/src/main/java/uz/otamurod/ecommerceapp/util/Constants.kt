b42f96da6c33afcca36538e1a9f8b1cb365438a1
otamurod <o.safarov@student.inha.uz>
2024-04-07 13:54:34 +0500

    feat: detect if IntroductionFragment is shown

--- app/src/main/java/uz/otamurod/ecommerceapp/util/Constants.kt
+++ app/src/main/java/uz/otamurod/ecommerceapp/util/Constants.kt
@@ -2,4 +2,6 @@ package uz.otamurod.ecommerceapp.util
 
 object Constants {
     const val USER_COLLECTION = "user"
+    const val INTRODUCTION_SHARED_PREFERENCES = "IntroductionSharedPreferences"
+    const val INTRODUCTION_BUTTON_CLICKED_KEY = "IntroductionButtonClickedKey"
 }
