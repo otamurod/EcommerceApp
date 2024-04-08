b317bb55e49e9fb443d2a1b9f785e817dbbb184d
otamurod <o.safarov@student.inha.uz>
2024-04-08 16:29:17 +0500

    feat: implement Pagination for RecyclerViews in MainCategoryFragment

--- app/src/main/java/uz/otamurod/ecommerceapp/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/uz/otamurod/ecommerceapp/fragments/categories/MainCategoryFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -29,8 +30,7 @@ class MainCategoryFragment : Fragment() {
     private lateinit var bestProductsAdapter: BestProductsAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainCategoryBinding.inflate(inflater)
         return binding.root
@@ -104,6 +104,12 @@ class MainCategoryFragment : Fragment() {
                 }
             }
         }
+
+        binding.nestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { view, _, scrollY, _, _ ->
+            if (view.getChildAt(0).bottom <= view.height + scrollY) {
+                viewModel.fetchBestProducts()
+            }
+        })
     }
 
     private fun hideLoading() {
